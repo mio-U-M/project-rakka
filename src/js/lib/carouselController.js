234ebd7bacd0b9f0201adacaebe4b2c249563f6c
tagami-mio <tagami-mio@kayac.com>
2018-12-31 00:24:31 +0900

    スライダーの中身を組みつつ、ボタンでの切り替えをできるようにした

--- src/js/lib/carouselController.js
+++ src/js/lib/carouselController.js
@@ -1,8 +1,45 @@
-import { TimelineMax } from 'gsap';
-import slideController from '@/js/lib/slideController';
+import { TimelineMax } from "gsap";
+import slideController from "@/js/lib/slideController";
 
 export default class carouselController {
-    constructor(carouselDom) {
-        this.carousel = carouselDom;
+    constructor(carouselElm) {
+        this.carousel = {
+            elm: carouselElm.querySelector(".js-slidecontainer"),
+            prevbtn: carouselElm.querySelector(".js-prev"),
+            nextbtn: carouselElm.querySelector(".js-next")
+        };
+        // 初期設定
+        this.currentSlide = 0;
+        this.slides = [];
+
+        this.init();
+    }
+
+    init() {
+        // スライドをセット
+        Array.from(document.querySelectorAll(".js-slide"), elm => {
+            return this.slides.push(new slideController(elm));
+        });
+        // 初期表示するスライドを設定
+        this.slides[this.currentSlide].enableCurent();
+
+        // ボタンの設定
+        this.carousel.prevbtn.addEventListener("click", () => {
+            this.slides[this.currentSlide].disableCurent();
+
+            let prevSlide = this.currentSlide - 1 < 0 ? this.slides.length - 1 : this.currentSlide - 1;
+
+            this.slides[prevSlide].enableCurent();
+            this.currentSlide = prevSlide;
+        });
+
+        this.carousel.nextbtn.addEventListener("click", () => {
+            this.slides[this.currentSlide].disableCurent();
+
+            let nextSlide = this.currentSlide + 1 < this.slides.length ? this.currentSlide + 1 : 0;
+
+            this.slides[nextSlide].enableCurent();
+            this.currentSlide = nextSlide;
+        });
     }
 }
--- src/js/lib/slideController.js
+++ src/js/lib/slideController.js
@@ -1,8 +1,21 @@
-import { TimelineMax } from 'gsap';
+import { TimelineMax } from "gsap";
 
 export default class slideController {
-    constructor() {
+    constructor(elm) {
+        this.slide = {
+            elm: elm,
+            container: elm.querySelector(".js-slidecontainer"),
+            img: elm.querySelector(".js-slideimg"),
+            no: elm.querySelector(".js-slideno"),
+            ttl: elm.querySelector("js-slidettl")
+        };
+    }
 
+    enableCurent() {
+        this.slide.elm.classList.add("curent-slide");
+    }
+
+    disableCurent() {
+        this.slide.elm.classList.remove("curent-slide");
     }
 }
-
