4fd059b8a62160d1b057290fe1e67a30daca8c86
tagami-mio <tagami-mio@kayac.com>
2019-01-19 09:45:18 +0900

    ホバーでスライドの色を変更

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,7 @@ import "ie-buster/dist/ie-buster.min.js";
 import carouselController from "@/js/lib/carouselController";
 import { shaderinit } from "@/js/lib/shaderController.js";
 import { TweenMax, TimelineMax } from "gsap/TweenMax";
+import EventEmitter from 'events';
 
 const opening = document.querySelector(".js-opening");
 const openingttl1 = document.querySelector(".js-openingttl1");
@@ -10,18 +11,45 @@ const openingttl2 = document.querySelector(".js-openingttl2");
 const openingsubttl = document.querySelector(".js-openingsubttl");
 const contents = document.querySelector(".js-contents");
 
-// carousel init
-const carousel = new carouselController(document.querySelector(".js-carousel"));
-// shader init
-shaderinit();
-
-
 const openingTl = new TimelineMax();
 let filterelm0 = { val: 10 };
 let filterelm1 = { val: 30 };
 let filterelm2 = { val: 30 };
 let filterelm3 = { val: 30 };
 
+// carousel init
+const carousel = new carouselController(document.querySelector(".js-carousel"));
+carousel.on('hover', () => {
+    TweenMax.to(filterelm0, 1.0, 
+        {
+            val: 0,
+            ease: Power4.easeOut,
+            onUpdate: () => 
+            TweenMax.set(opening, { 
+                webkitFilter: "grayscale(" + filterelm0.val + ")",
+                filter:"grayscale(" +  filterelm0.val + ")"}
+            )
+        }
+    )
+});
+carousel.on('hoverout', () => {
+    TweenMax.to(filterelm0, 1.0, 
+        {
+            val: 10,
+            ease: Power4.easeOut,
+            onUpdate: () => 
+            TweenMax.set(opening, { 
+                webkitFilter: "grayscale(" + filterelm0.val + ")",
+                filter:"grayscale(" +  filterelm0.val + ")"}
+            )
+        }
+    )
+});
+
+// shader init
+shaderinit();
+
+// opening animation
 openingTl
     // initialize
     .set(opening, { opacity: 0 }, 0)
